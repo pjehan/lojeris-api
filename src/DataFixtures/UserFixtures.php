38d4e9096863becfe4b672e23a41633646648c90
pjehan <pierre.jehan@gmail.com>
2021-11-19 11:09:18 +0100

    Upgrade to Symfony 5.3

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
@@ -15,7 +15,7 @@ class UserFixtures extends Fixture
     /**
      * UserFixtures constructor.
      */
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    public function __construct(UserPasswordHasherInterface $encoder)
     {
         $this->encoder = $encoder;
     }
@@ -24,14 +24,14 @@ class UserFixtures extends Fixture
     {
         $admin = new User();
         $admin->setEmail("pierre.jehan@gmail.com");
-        $admin->setPassword($this->encoder->encodePassword($admin, "pjehan"));
+        $admin->setPassword($this->encoder->hashPassword($admin, "pjehan"));
         $admin->setRoles(["ROLE_ADMIN"]);
         $manager->persist($admin);
         $this->addReference("user-admin", $admin);
 
         $user = new User();
         $user->setEmail("john.doe@gmail.com");
-        $user->setPassword($this->encoder->encodePassword($admin, "john"));
+        $user->setPassword($this->encoder->hashPassword($admin, "john"));
         $manager->persist($user);
         $this->addReference("user-user", $user);
 
